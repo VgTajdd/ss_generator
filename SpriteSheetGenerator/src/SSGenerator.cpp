28a1567902886d91477ee87c759a593e79ae3588
VgTajdd <aduranddiaz@gmail.com>
2020-05-31 20:09:40 -0500

    [WIP] Implement SSGenerator::generateSpriteSheets method
    Completed automaticSize generation

--- SpriteSheetGenerator/src/SSGenerator.cpp
+++ SpriteSheetGenerator/src/SSGenerator.cpp
@@ -2,10 +2,26 @@
 
 #include <QImage>
 #include <QDebug>
+#include <QPainter>
+#include <QDateTime>
 
 Node* SSGenerator::root = nullptr;
 
-void SSGenerator::fit( std::vector< Image >& images )
+Node::~Node()
+{
+	if ( right != nullptr )
+	{
+		delete right;
+		right = nullptr;
+	}
+	if ( down != nullptr )
+	{
+		delete down;
+		down = nullptr;
+	}
+}
+
+void SSGenerator::fit( std::vector< Image >& images, const bool automaticSize )
 {
 	for ( int n = 0; n < images.size(); n++ )
 	{
@@ -13,6 +29,17 @@ void SSGenerator::fit( std::vector< Image >& images )
 		if ( auto node = findNode( root, block.w, block.h ) )
 		{
 			block.fit = splitNode( node, block.w, block.h );
+		}
+		else
+		{
+			if ( automaticSize )
+			{
+				block.fit = growNode( block.w, block.h );
+			}
+			else
+			{
+				// Create new root;
+			}
 		}
 	}
 }
@@ -53,14 +80,83 @@ Node* SSGenerator::splitNode( Node* node, float w, float h )
 	return node;
 }
 
+Node* SSGenerator::growNode( float w, float h )
+{
+	bool canGrowDown = ( w <= root->w );
+	bool canGrowRight = ( h <= root->h );
+
+	bool shouldGrowRight = canGrowRight && ( root->h >= ( root->w + w ) ); // attempt to keep square-ish by growing right when height is much greater than width
+	bool shouldGrowDown = canGrowDown && ( root->w >= ( root->h + h ) ); // attempt to keep square-ish by growing down  when width  is much greater than height
+
+	if ( shouldGrowRight ) return growRight( w, h );
+	else if ( shouldGrowDown ) return growDown( w, h );
+	else if ( canGrowRight ) return growRight( w, h );
+	else if ( canGrowDown ) return growDown( w, h );
+	else return nullptr; // need to ensure sensible root starting size to avoid this happening
+}
+
+Node* SSGenerator::growRight( float w, float h )
+{
+	Node* newRoot = new Node();
+	newRoot->used = true;
+	newRoot->x = 0;
+	newRoot->y = 0;
+	newRoot->w = root->w + w;
+	newRoot->h = root->h;
+	Node* right = new Node();
+	right->x = root->w;
+	right->w = w;
+	right->h = root->h;
+	newRoot->down = root;
+	newRoot->right = right;
+
+	root = newRoot;
+
+	if ( auto node = findNode( root, w, h ) )
+	{
+		return splitNode( node, w, h );
+	}
+	else
+	{
+		return nullptr;
+	}
+}
+
+Node* SSGenerator::growDown( float w, float h )
+{
+	Node* newRoot = new Node();
+	newRoot->used = true;
+	newRoot->x = 0;
+	newRoot->y = 0;
+	newRoot->w = root->w;
+	newRoot->h = root->h + h;
+	Node* down = new Node();
+	down->y = root->h;
+	down->w = root->w;
+	down->h = h;
+	newRoot->down = down;
+	newRoot->right = root;
+
+	root = newRoot;
+
+	if ( auto node = findNode( root, w, h ) )
+	{
+		return splitNode( node, w, h );
+	}
+	else
+	{
+		return nullptr;
+	}
+}
+
 bool SSGenerator::generateSpriteSheets( const std::vector< QString >& filenames,
 										const QString& folderPath,
 										const bool automaticSize,
 										const QSize& fixedSize )
 {
-	// Sort by width.
-
 	std::vector< Image > images;
+
+	int maxWidth = 0, maxHeight = 0;
 
 	bool loadImagesFailed = false;
 	for ( const auto& filename : filenames )
@@ -76,6 +172,8 @@ bool SSGenerator::generateSpriteSheets( const std::vector< QString >& filenames,
 		imgObject.filename = filename;
 		imgObject.w = img.width();
 		imgObject.h = img.height();
+		maxWidth = std::max( maxWidth, imgObject.w );
+		maxHeight = std::max( maxHeight, imgObject.h );
 		images.push_back( imgObject );
 	}
 
@@ -94,10 +192,48 @@ bool SSGenerator::generateSpriteSheets( const std::vector< QString >& filenames,
 	}
 
 	root = new Node();
-	root->w = 1024;
-	root->h = 1024;
+	if ( !automaticSize )
+	{
+		if ( maxWidth > fixedSize.width() || maxHeight > fixedSize.height() )
+		{
+			delete root;
+			root = nullptr;
+			return false;
+		}
+		root->w = fixedSize.width();
+		root->h = fixedSize.height();
+	}
+	else
+	{
+		root->w = images[0].w;
+		root->h = images[0].h;
+	}
 
-	fit( images );
+	fit( images, automaticSize );
+
+	// TODO :
+	// Remove old atlas.
+
+	QString fmt = "_yyyyMMdd_hhmmss";
+	QString time = QDateTime::currentDateTime().toString( fmt );
+
+	QImage atlas( QSize( root->w, root->h ), QImage::Format_RGB32 );
+	atlas.fill( QColor( 0, 0, 0, 0 ) );
+	QPainter painter( &atlas );
+	for ( const auto& image : images )
+	{
+		// Create atlas.
+		QString imgPath( folderPath + "/" + image.filename );
+		painter.drawImage( QPoint( image.fit->x, image.fit->y ), QImage( imgPath ) );
+	}
+	painter.end();
+	atlas.save( folderPath + "/atlas" + time + ".png", "PNG" );
+
+	if ( root )
+	{
+		delete root;
+		root = nullptr;
+	}
 
 	return true;
 }
--- SpriteSheetGenerator/src/SSGenerator.h
+++ SpriteSheetGenerator/src/SSGenerator.h
@@ -7,6 +7,7 @@
 
 struct Node
 {
+	~Node();
 	float x = 0;
 	float y = 0;
 	float w = 0;
@@ -20,16 +21,19 @@ struct Image
 {
 	QString filename;
 	Node* fit = nullptr;
-	float w = 0;
-	float h = 0;
+	int w = 0;
+	int h = 0;
 };
 
 class SSGenerator
 {
 	static Node* root;
-	static void fit( std::vector< Image >& images );
+	static void fit( std::vector< Image >& images, const bool automaticSize );
 	static Node* findNode( Node* root, float w, float h );
 	static Node* splitNode( Node* node, float w, float h );
+	static Node* growNode( float w, float h );
+	static Node* growRight( float w, float h );
+	static Node* growDown( float w, float h );
 public:
 	static bool generateSpriteSheets( const std::vector<QString>& filenames, const QString& folderPath, const bool automaticSize, const QSize& fixedSize );
 };
